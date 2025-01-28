10d52d86ac3f106acf2f22d354ede9cb9242566e
NyiNyiZin-cmd <nyinyizin951@gmail.com>
2025-01-28 16:47:16 +0900

    calculation one

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,50 @@
+import { useState } from "react";
+
+import Buttons from "./components/Buttons";
+import Input from "./components/Input";
+import Output from "./components/Output";
+
 function App() {
+  const [output, setOutput] = useState(0);
+  const [input, setInput] = useState(0);
+
+  const handleCalculation = (value) => {
+    switch (value) {
+      case "C":
+        setOutput(0);
+        setInput(0);
+        break;
+
+      case "AC":
+        input.length == 1
+          ? setInput(0)
+          : setInput(input.slice(0, input.length - 1));
+        break;
+
+      case "=":
+        try {
+          setOutput(eval(input));
+          setInput(0);
+        } catch (e) {
+          setOutput(e.message);
+        }
+        break;
+
+      default:
+        input == 0 ? setInput(value) : setInput(input + value);
+    }
+  };
+
   return (
-   <>
-    <div className="text-3xl font-bold underline text-blue-400">
-      Hello Calculator
-    </div>
-    <h3 className="text-6xl">Hello</h3>
-    <h3 className="text-3xl text-amber-400">Hello</h3>
-   </>
+    <>
+      <div className="w-full h-screen bg-gray-700 flex flex-col items-center justify-center">
+        <div className="md:w-1/2 mx-auto">
+          <Output output={output} />
+          <Input input={input} />
+          <Buttons handleCalculation={handleCalculation} />
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Input.jsx
@@ -0,0 +1,9 @@
+const Input = ({input}) => {
+  return (
+    <div className="w-full bg-gray-800 py-3 px-4 text-right text-white ">
+      {input}
+    </div>
+  );
+};
+
+export default Input;
