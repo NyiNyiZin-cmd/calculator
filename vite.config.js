b3b3e6d1ff51805ca9a5b2a3bd12bd4920da7ad6
NyiNyiZin-cmd <nyinyizin951@gmail.com>
2025-01-28 17:20:07 +0900

    adding some

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/calculator',
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
