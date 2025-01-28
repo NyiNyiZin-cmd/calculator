99ba02b171bc7cd8c969ad99de219a21cb1b8e2c
NyiNyiZin-cmd <nyinyizin951@gmail.com>
2025-01-28 15:38:06 +0900

    tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 function App() {
-  return <div className="text-3xl font-bold underline text-blue-400">
-    Hello Calculator
-    </div>;
+  return (
+   <>
+    <div className="text-3xl font-bold underline text-blue-400">
+      Hello Calculator
+    </div>
+    <h3 className="text-6xl">Hello</h3>
+    <h3 className="text-3xl text-amber-400">Hello</h3>
+   </>
+  );
 }
 
 export default App;
